3acd1794e0d30ac001f7dd034ead3682f044470d
nametake <nametake.kyarabuki@gmail.com>
2023-08-12 00:12:07 +0900

    Added manifest files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'Shopify Favicon Changer',
+  version: '0.0.1',
+  action: {
+    default_popup: 'index.html',
+  }
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })]
 })
