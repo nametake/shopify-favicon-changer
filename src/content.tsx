47ca14000bd28ceeaffeb0d92c6aa6ff1ee86b7f
nametake <nametake.kyarabuki@gmail.com>
2023-08-12 13:06:56 +0900

    Added icons

--- src/content.tsx
+++ src/content.tsx
@@ -1,34 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import defaultIcon from './assets/default.ico'
-// import reactLogo from './assets/react.svg'
-import { share } from './share'
+import redIcon from './assets/red.png'
 
-share()
-
-const root = document.createElement("div");
-root.id = "crx-root";
-document.body.appendChild(root);
-
-console.log("foo")
-
-// const link = document.querySelector("link[rel~='icon']");
-// if (!link) {
-//   link = document.createElement('link');
-//   link.rel = 'icon';
-//   document.head.appendChild(link);
-// }
-// if (link) {
-//   link.href = chrome.runtime.getURL(reactLogo);
-// }
-
-console.log(defaultIcon)
-console.log(chrome.runtime.getURL(defaultIcon))
-
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const link = document.querySelector("link[rel~='icon']");
+if (link instanceof HTMLLinkElement) {
+  link.href = chrome.runtime.getURL(redIcon);
+}
