b9b22fef7f3ce57bc95a7919682575b135a1f9dc
nametake <nametake.kyarabuki@gmail.com>
2023-08-13 18:44:58 +0900

    Set store icons

--- src/contents/admin.ts
+++ src/contents/admin.ts
@@ -1,13 +1,22 @@
 import { icons } from '@/icons';
 import { getStore } from '@/store';
 
+const getStoreName = () => {
+  const url = new URL(location.href);
+  return url.pathname.split('/')[2];
+};
+
 const main = async () => {
   const store = await getStore();
-  if (store.adminDefaultIcon === 'DEFAULT') {
+  const storeName = getStoreName();
+  const storeIcon =
+    store.storeIcons.find((icon) => icon.name === storeName)?.type ??
+    store.adminDefaultIcon;
+  if (storeIcon === 'DEFAULT') {
     return;
   }
 
-  const iconLink = icons.get(store.adminDefaultIcon);
+  const iconLink = icons.get(storeIcon);
   const link = document.querySelector("link[rel~='icon']");
   if (link instanceof HTMLLinkElement && iconLink) {
     link.href = chrome.runtime.getURL(iconLink);
