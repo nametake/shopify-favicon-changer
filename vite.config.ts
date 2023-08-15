067d790edd5f92b6ab09b7f1ce06d1487f28825b
nametake <nametake.kyarabuki@gmail.com>
2023-08-16 01:43:59 +0900

    Added github actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { crx, defineManifest } from '@crxjs/vite-plugin';
 const manifest = defineManifest({
   manifest_version: 3,
   name: 'Shopify Favicon Changer',
-  version: '0.0.1',
+  version: process.env.RELEASE_VERSION,
   action: {
     default_popup: 'index.html',
     default_icon: 'icon.png',
